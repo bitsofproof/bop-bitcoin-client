1c21a9ca356dbe12455437c03b10e3582e7b3530
Bártfai Tamás <tamas.bartfai@bitsofproof.com>
2014-06-04 12:26:31 +0200

    Moved new JMSConnector here

---
+++ jms/src/main/java/com/bitsofproof/supernode/connector4/jms/JMSConnector.java
@@ -0,0 +1,703 @@
+package com.bitsofproof.supernode.connector4.jms;
+
+import com.bitsofproof.supernode.connector4.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.*;
+
+public class JMSConnector implements Connector4
+{
+	private static final Logger log = LoggerFactory.getLogger (JMSConnector.class);
+
+	private final ResponseWorker responseWorker;
+
+	private final ConsumerProducerCache consumerProducerCache;
+
+	private final Map<String, ? extends Destination> destinations;
+
+	private final Connection connection;
+
+	private final ExecutorService requestHandlerExecutor;
+
+	private final ExecutorService requestSenderExecutor;
+
+	private final TemporaryQueue responseQueue;
+
+	public JMSConnector (ConnectionFactory connectionFactory,
+	                     Map<String, ? extends Destination> destinations,
+	                     ExecutorService requestHandlerExecutor,
+	                     ExecutorService requestSenderExecutor) throws Connector4Exception
+	{
+		try
+		{
+			this.connection = connectionFactory.createConnection ();
+			Session session = connection.createSession (false, Session.AUTO_ACKNOWLEDGE);
+
+			this.responseQueue = session.createTemporaryQueue ();
+
+			this.requestHandlerExecutor = requestHandlerExecutor;
+			this.requestSenderExecutor = requestSenderExecutor;
+			this.destinations = destinations;
+			this.consumerProducerCache = new ConsumerProducerCache (session, destinations);
+
+			this.responseWorker = new ResponseWorker (connection, destinations);
+		}
+		catch (JMSException e)
+		{
+			throw new Connector4Exception (e);
+		}
+	}
+
+	// public API methods
+
+	@Override
+	public void publish (String topic, byte[] bytes) throws Connector4Exception
+	{
+		Destination destination = destinations.get (topic);
+		sendMessage (new OutputMessage (bytes, null, destination));
+	}
+
+	@Override
+	public void setSubscription (String topic, final Subscription l) throws Connector4Exception
+	{
+		try
+		{
+			MessageConsumer consumer = consumerProducerCache.getConsumer (topic);
+			consumer.setMessageListener (new RequestHandlerMessageListener ()
+			{
+				@Override
+				public void handleRequest (byte[] bytes, Destination replyTo, String correlationId)
+				{
+					l.onMessage (bytes);
+				}
+			});
+		}
+		catch (JMSException e)
+		{
+			throw new Connector4Exception (e);
+		}
+	}
+
+	@Override
+	public void setRequestHandler (String queue, final RequestHandler h) throws Connector4Exception
+	{
+		try
+		{
+			MessageConsumer consumer = consumerProducerCache.getConsumer (queue);
+			consumer.setMessageListener (new RequestHandlerMessageListener ()
+			{
+				@Override
+				public void handleRequest (byte[] bytes, Destination replyTo, String correlationId)
+				{
+					requestHandlerExecutor.execute (new RequestHandlerTask (JMSConnector.this, h, bytes, replyTo, correlationId));
+				}
+			});
+		}
+		catch (JMSException e)
+		{
+			log.error ("Error listening to " + queue, e);
+			throw new Connector4Exception (e);
+		}
+	}
+
+	@Override
+	public void setRequestHandler (String queue, final MultipartResponseRequestHandler h) throws Connector4Exception
+	{
+		try
+		{
+			MessageConsumer consumer = consumerProducerCache.getConsumer (queue);
+			consumer.setMessageListener (new RequestHandlerMessageListener ()
+			{
+				@Override
+				public void handleRequest (byte[] bytes, Destination replyTo, String correlationId)
+				{
+					requestHandlerExecutor.execute (new MultipartRequestWorker (JMSConnector.this, h, bytes, replyTo, correlationId));
+				}
+			});
+		}
+		catch (JMSException e)
+		{
+			throw new Connector4Exception (e);
+		}
+	}
+
+	@Override
+	public byte[] request (String queue, byte[] request) throws Connector4Exception
+	{
+		return request (queue, request, 0, TimeUnit.MILLISECONDS);
+	}
+
+	@Override
+	public byte[] request (String queue, byte[] request, long timeout, TimeUnit unit) throws Connector4Exception
+	{
+		Destination destination = destinations.get (queue);
+		if ( destination == null )
+		{
+			throw new Connector4Exception ("Unknown destination");
+		}
+
+		return executeRequest (new SimpleRequestSenderTask (connection,
+		                                                    responseQueue,
+		                                                    new OutputMessage (request, UUID.randomUUID ().toString (), destination),
+		                                                    timeout, unit));
+	}
+
+	@Override
+	public byte[][] multipartRequest (String queue, byte[] request) throws Connector4Exception
+	{
+		return multipartRequest (queue, request, 0, TimeUnit.MILLISECONDS);
+	}
+
+	@Override
+	public byte[][] multipartRequest (String queue, byte[] request, long timeout, TimeUnit unit) throws Connector4Exception
+	{
+		Destination destination = destinations.get (queue);
+		if ( destination == null )
+		{
+			throw new Connector4Exception ("Unknown destination");
+		}
+
+		return executeRequest (new MultipartResponseRequestSenderTask (connection,
+		                                                               responseQueue,
+		                                                               new OutputMessage (request, UUID.randomUUID ().toString (), destination),
+		                                                               timeout, unit));
+	}
+
+	@Override
+	public void start () throws Connector4Exception
+	{
+		try
+		{
+			responseWorker.start ();
+			connection.start ();
+		}
+		catch (JMSException e)
+		{
+			throw new Connector4Exception (e);
+		}
+	}
+
+	@Override
+	public void close () throws Connector4Exception
+	{
+		try
+		{
+			responseWorker.shutdown ();
+			shutdownExecutor (requestHandlerExecutor);
+			shutdownExecutor (requestSenderExecutor);
+			connection.close ();
+		}
+		catch (JMSException e)
+		{
+			throw new Connector4Exception (e);
+		}
+	}
+
+	@Override
+	public void stop () throws Connector4Exception
+	{
+		try
+		{
+			connection.stop ();
+		}
+		catch (JMSException e)
+		{
+			throw new Connector4Exception (e);
+		}
+	}
+
+	// generic utility methods and classes
+
+	private void shutdownExecutor (ExecutorService executor)
+	{
+		executor.shutdown ();
+		try
+		{
+			executor.awaitTermination (1, TimeUnit.SECONDS);
+		}
+		catch (InterruptedException ignored)
+		{
+		}
+		if ( !executor.isTerminated () )
+		{
+			executor.shutdownNow ();
+		}
+	}
+
+	/**
+	 * Sends a response message asynchonrously, where the replyTo shouldn't be set.
+	 */
+	private void sendMessage (OutputMessage r)
+	{
+		responseWorker.send (r);
+	}
+
+	/**
+	 * Caching producers and consumers for long-lived JMS sessions.
+	 */
+	public static class ConsumerProducerCache
+	{
+		private final Session session;
+
+		private final ConcurrentHashMap<Destination, MessageProducer> producers;
+
+		private final ConcurrentHashMap<Destination, MessageConsumer> consumers;
+
+		private final Map<String, ? extends Destination> destinations;
+
+
+		public ConsumerProducerCache (Session session, Map<String, ? extends Destination> destinations)
+		{
+			this.session = session;
+			this.destinations = destinations;
+			this.producers = new ConcurrentHashMap<> ();
+			this.consumers = new ConcurrentHashMap<> ();
+		}
+
+		public MessageConsumer getConsumer (String name) throws JMSException, Connector4Exception
+		{
+			Destination destination = destinations.get (name);
+			if ( destination == null )
+			{
+				throw new Connector4Exception ("Unknown destination " + name);
+			}
+
+			return getConsumer (destination);
+		}
+
+		public MessageConsumer getConsumer (Destination destination) throws JMSException
+		{
+			MessageConsumer consumer = consumers.get (destination);
+			if ( consumer == null )
+			{
+
+				MessageConsumer newConsumer = session.createConsumer (destination);
+				consumer = consumers.putIfAbsent (destination, newConsumer);
+				if ( consumer == null )
+				{
+					consumer = newConsumer;
+				}
+				else
+				{
+					newConsumer.close ();
+				}
+			}
+
+			return consumer;
+		}
+
+		public MessageProducer getProducer (String name) throws JMSException, Connector4Exception
+		{
+			Destination destination = destinations.get (name);
+			if ( destination == null )
+			{
+				throw new Connector4Exception ("Unknown destination " + name);
+			}
+
+			return getProducer (destination);
+		}
+
+		public MessageProducer getProducer (Destination destination) throws JMSException
+		{
+			MessageProducer producer = producers.get (destination);
+			if ( producer == null )
+			{
+				MessageProducer newProducer = session.createProducer (destination);
+				producer = producers.putIfAbsent (destination, newProducer);
+				if ( producer == null )
+				{
+					producer = newProducer;
+				}
+				else
+				{
+					newProducer.close ();
+				}
+			}
+
+			return producer;
+		}
+	}
+
+	/**
+	 * Value object representing a message sent to a gived destination with an optional correlationId.
+	 */
+	private static class OutputMessage
+	{
+		final String correlationId;
+
+		final Destination destination;
+
+		final byte[] bytes;
+
+		private OutputMessage (byte[] bytes, String correlationId, Destination destination)
+		{
+			this.bytes = bytes;
+			this.destination = destination;
+			this.correlationId = correlationId;
+		}
+
+		private BytesMessage createJMSMessage (Session session) throws JMSException
+		{
+			BytesMessage message = session.createBytesMessage ();
+			if ( correlationId != null )
+			{
+				message.setJMSCorrelationID (correlationId);
+			}
+
+			if ( bytes != null )
+			{
+				message.writeBytes (bytes);
+			}
+
+			return message;
+		}
+
+	}
+
+
+	private static class ResponseWorker extends Thread
+	{
+		private static final Logger log = LoggerFactory.getLogger (ResponseWorker.class);
+
+		private final LinkedBlockingQueue<OutputMessage> outputMessageQueue = new LinkedBlockingQueue<> ();
+
+		private final Session session;
+
+		private final ConsumerProducerCache producerCache;
+
+		private ResponseWorker (Connection connection, Map<String, ? extends Destination> destinations) throws JMSException
+		{
+			super ("connector-response-worker");
+			this.session = connection.createSession (false, Session.AUTO_ACKNOWLEDGE);
+			this.producerCache = new ConsumerProducerCache (session, destinations);
+			setDaemon (true);
+		}
+
+		public void shutdown ()
+		{
+			this.interrupt ();
+
+			try
+			{
+				this.join (TimeUnit.SECONDS.toMillis (1));
+			}
+			catch (InterruptedException ignored)
+			{
+			}
+		}
+
+		public void send (OutputMessage outputMessage)
+		{
+			try
+			{
+				outputMessageQueue.put (outputMessage);
+			}
+			catch (InterruptedException ignored)
+			{
+			}
+		}
+
+		@Override
+		public void run ()
+		{
+			try
+			{
+				while (!Thread.currentThread ().isInterrupted ())
+				{
+					try
+					{
+						OutputMessage outputMessage = outputMessageQueue.take ();
+
+						BytesMessage message = outputMessage.createJMSMessage (session);
+
+						MessageProducer producer = producerCache.getProducer (outputMessage.destination);
+						producer.send (message);
+					}
+					catch (InterruptedException ignored)
+					{
+					}
+					catch (JMSException e)
+					{
+						log.error ("Error sending response", e);
+					}
+				}
+			}
+			finally
+			{
+				log.info ("Shutting down response worker");
+
+				try
+				{
+					session.close ();
+				}
+				catch (JMSException ignored)
+				{
+				}
+			}
+		}
+	}
+
+	private static class RequestHandlerTask implements Runnable
+	{
+
+		private final JMSConnector connector;
+
+		private final RequestHandler handler;
+
+		private final byte[] request;
+
+		private final Destination replyTo;
+
+		private final String correlationId;
+
+		private RequestHandlerTask (JMSConnector connector, RequestHandler handler, byte[] request, Destination replyTo, String correlationId)
+		{
+			this.connector = connector;
+			this.handler = handler;
+			this.request = request;
+			this.replyTo = replyTo;
+			this.correlationId = correlationId;
+		}
+
+		@Override
+		public void run ()
+		{
+			byte[] responseBytes = handler.onRequest (request);
+
+			connector.sendMessage (new OutputMessage (responseBytes, correlationId, replyTo));
+		}
+	}
+
+	private static class MultipartRequestWorker implements Runnable
+	{
+		private final JMSConnector connector;
+
+		private final MultipartResponseRequestHandler handler;
+
+		private final byte[] request;
+
+		private final Destination replyTo;
+
+		private final String correlationId;
+
+		private MultipartRequestWorker (JMSConnector connector, MultipartResponseRequestHandler handler, byte[] request, Destination replyTo,
+		                                String correlationId)
+		{
+			this.connector = connector;
+			this.handler = handler;
+			this.request = request;
+			this.replyTo = replyTo;
+			this.correlationId = correlationId;
+		}
+
+		@Override
+		public void run ()
+		{
+			handler.onRequest (request, new MultipartResponse ()
+			{
+				@Override
+				public void send (byte[] bytes)
+				{
+					connector.sendMessage (new OutputMessage (bytes, correlationId, replyTo));
+				}
+
+				@Override
+				public void eof ()
+				{
+					connector.sendMessage (new OutputMessage (null, correlationId, replyTo));
+				}
+			});
+		}
+	}
+
+	public static abstract class RequestHandlerMessageListener implements MessageListener
+	{
+		@Override
+		public void onMessage (Message message)
+		{
+			try
+			{
+				if ( message instanceof BytesMessage )
+				{
+					BytesMessage bm = (BytesMessage) message;
+					long len = bm.getBodyLength ();
+					if ( len < Integer.MAX_VALUE )
+					{
+						byte[] bytes = new byte[(int) len];
+						bm.readBytes (bytes);
+						Destination replyTo = bm.getJMSReplyTo ();
+						String correlationId = bm.getJMSCorrelationID ();
+
+						handleRequest (bytes, replyTo, correlationId);
+					}
+					else
+					{
+						log.error ("Message size too big: ", len);
+					}
+				}
+			}
+			catch (JMSException e)
+			{
+				log.error ("Error handling request", e);
+			}
+		}
+
+		public abstract void handleRequest (byte[] bytes, Destination replyTo, String correlationId);
+
+	}
+
+	private static abstract class RequestSenderTask<T> implements Callable<T>
+	{
+		private final OutputMessage request;
+
+		private final Connection connection;
+
+		private final Destination responseTo;
+
+		protected final long timeout;
+
+		protected RequestSenderTask (Connection connection, Destination responseTo, OutputMessage request, long timeout, TimeUnit unit)
+		{
+			this.request = request;
+			this.connection = connection;
+			this.responseTo = responseTo;
+			this.timeout = unit.toMillis (timeout);
+		}
+
+		@Override
+		public T call () throws Exception
+		{
+			Session session = connection.createSession (false, Session.AUTO_ACKNOWLEDGE);
+
+			BytesMessage message = request.createJMSMessage (session);
+			message.setJMSReplyTo (responseTo);
+			MessageConsumer responseConsumer = session.createConsumer (responseTo, "JMSCorrelationID = '" + message.getJMSCorrelationID () + "'");
+
+			MessageProducer producer = session.createProducer (request.destination);
+			producer.send (message);
+
+			return handleResponse (responseConsumer);
+		}
+
+		protected abstract T handleResponse (MessageConsumer responseConsumer) throws JMSException, Connector4Exception;
+
+		protected byte[] toBytes (BytesMessage bm) throws Connector4Exception, JMSException
+		{
+			long len = bm.getBodyLength ();
+			if ( len == 0 )
+			{
+				return null;
+			}
+			else if ( len < Integer.MAX_VALUE )
+			{
+				byte[] bytes = new byte[(int) len];
+				bm.readBytes (bytes);
+
+				return bytes;
+			}
+			else
+			{
+				throw new Connector4Exception ("Message size too big: " + len);
+			}
+		}
+	}
+
+	private static class SimpleRequestSenderTask extends RequestSenderTask<byte[]>
+	{
+
+		protected SimpleRequestSenderTask (Connection connection, Destination responseTo, OutputMessage request, long timeout, TimeUnit unit)
+		{
+			super (connection, responseTo, request, timeout, unit);
+		}
+
+		@Override
+		protected byte[] handleResponse (MessageConsumer responseConsumer) throws JMSException, Connector4Exception
+		{
+			Message response = responseConsumer.receive (timeout);
+			if ( response == null )
+			{
+				throw new Connector4Exception ("Response timed out");
+			}
+			else if ( response instanceof BytesMessage )
+			{
+				return toBytes ((BytesMessage) response);
+			}
+			else
+			{
+				throw new Connector4Exception ("Unable to handle message " + response);
+			}
+		}
+	}
+
+	private static class MultipartResponseRequestSenderTask extends RequestSenderTask<byte[][]>
+	{
+
+		MultipartResponseRequestSenderTask (Connection connection, Destination responseTo, OutputMessage request, long timeout, TimeUnit unit)
+		{
+			super (connection, responseTo, request, timeout, unit);
+		}
+
+		@Override
+		protected byte[][] handleResponse (MessageConsumer responseConsumer) throws JMSException, Connector4Exception
+		{
+			ArrayList<BytesMessage> responses = new ArrayList<> ();
+
+			BytesMessage bm;
+			do
+			{
+				Message response = responseConsumer.receive (TimeUnit.SECONDS.toMillis (2));
+				// TODO replace Preconditions with Connector4Exception
+				if ( response instanceof BytesMessage )
+				{
+					bm = (BytesMessage) response;
+					if ( bm.getBodyLength () != 0 )
+					{
+						responses.add (bm);
+					}
+				}
+				else
+				{
+					throw new Connector4Exception ("Invalid JMS message");
+				}
+
+			} while (bm.getBodyLength () != 0);
+
+			byte[][] result = new byte[responses.size ()][];
+			for (int i = 0; i < responses.size (); i++)
+			{
+				result[i] = toBytes (responses.get (i));
+			}
+
+			return result;
+		}
+	}
+
+	private <T> T executeRequest (Callable<T> task) throws Connector4Exception
+	{
+		try
+		{
+			return requestSenderExecutor.submit (task).get ();
+		}
+		catch (InterruptedException e)
+		{
+			throw new Connector4Exception (e);
+		}
+		catch (ExecutionException e)
+		{
+			Throwable cause = e.getCause ();
+			if ( cause instanceof Connector4Exception )
+			{
+				throw (Connector4Exception) cause;
+			}
+
+			throw new Connector4Exception (cause);
+		}
+
+	}
+
+}
---
+++ jms/src/test/java/com/bitsofproof/supernode/connector4/jms/JMSConnectorTest.java
@@ -0,0 +1,305 @@
+package com.bitsofproof.supernode.connector4.jms;
+
+import com.bitsofproof.supernode.connector4.*;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableMap;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.command.ActiveMQDestination;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.apache.activemq.store.memory.MemoryPersistenceAdapter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.jms.ConnectionFactory;
+import java.util.*;
+import java.util.concurrent.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+// TODO test response where the response byte[] should be null
+public class JMSConnectorTest
+{
+	private BrokerService broker;
+
+	private JMSConnector clientConnector;
+
+	private JMSConnector serverConnector;
+
+	private ExecutorService requestExecutor;
+
+	@Before
+	public void setUp () throws Exception
+	{
+		broker = new BrokerService ();
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory ("vm://localhost");
+
+
+		ImmutableMap<String, ActiveMQDestination> destinations = ImmutableMap.of ("commandQueue", new ActiveMQQueue ("commandQueue"),
+		                                                                          "requestQueue", new ActiveMQQueue ("requestQueue"),
+		                                                                          "topic", new ActiveMQTopic ("topic"),
+		                                                                          "requestTopic", new ActiveMQTopic ("requestTopic"));
+
+		broker.setDestinations (destinations.values ().toArray (new ActiveMQDestination[destinations.size ()]));
+		broker.setPersistent (false);
+		broker.setPersistenceAdapter (new MemoryPersistenceAdapter ());
+		broker.start ();
+
+		clientConnector = new JMSConnector (connectionFactory,
+		                                    destinations,
+		                                    Executors.newCachedThreadPool (),
+		                                    Executors.newCachedThreadPool ());
+		serverConnector = new JMSConnector (connectionFactory,
+		                                    destinations,
+		                                    Executors.newCachedThreadPool (),
+		                                    Executors.newCachedThreadPool ());
+
+		requestExecutor = Executors.newCachedThreadPool ();
+	}
+
+	@After
+	public void tearDown () throws Exception
+	{
+		clientConnector.close ();
+		serverConnector.close ();
+
+		broker.stop ();
+
+		requestExecutor.shutdownNow ();
+	}
+
+	@Test
+	public void testPubSub () throws Exception
+	{
+		final Set<String> messages = Collections.synchronizedSet (new HashSet<String> ());
+
+		clientConnector.setSubscription ("topic", new Subscription ()
+		{
+			@Override
+			public void onMessage (byte[] bytes)
+			{
+				messages.add (new String (bytes));
+			}
+		});
+
+		serverConnector.start ();
+		clientConnector.start ();
+
+		for (int i = 0; i < 100; i++)
+		{
+			final String message = "message" + i;
+			requestExecutor.execute (new Runnable ()
+			{
+				@Override
+				public void run ()
+				{
+					try
+					{
+						serverConnector.publish ("topic", message.getBytes ());
+					}
+					catch (Connector4Exception ignored)
+					{
+					}
+				}
+			});
+		}
+
+		clientConnector.close ();
+		serverConnector.close ();
+
+		for (int i = 0; i < 100; i++)
+		{
+			assertThat (messages).contains ("message" + i);
+		}
+	}
+
+	@Test
+	public void testTimingOutMultipartRequest () throws Exception
+	{
+		serverConnector.setRequestHandler ("requestQueue", new MultipartResponseRequestHandler ()
+		{
+
+			@Override
+			public void onRequest (byte[] request, MultipartResponse response)
+			{
+				try
+				{
+					Thread.sleep (1000);
+				}
+				catch (InterruptedException ignored)
+				{
+				}
+
+				response.send (request);
+				response.send (request);
+				response.eof ();
+			}
+		});
+
+		serverConnector.start ();
+		clientConnector.start ();
+
+		try
+		{
+			clientConnector.request ("requestQueue", "still waiting".getBytes (), 100, TimeUnit.MILLISECONDS);
+			fail ("Should've timed out");
+		}
+		catch (Connector4Exception e)
+		{
+		}
+
+
+		clientConnector.close ();
+		serverConnector.close ();
+	}
+
+	@Test
+	public void testMultipartRequest () throws Exception
+	{
+		serverConnector.setRequestHandler ("requestQueue", new MultipartResponseRequestHandler ()
+		{
+			@Override
+			public void onRequest (byte[] request, MultipartResponse response)
+			{
+				String req = new String (request);
+				response.send ((req + "1").getBytes ());
+				response.send ((req + "2").getBytes ());
+				response.send ((req + "3").getBytes ());
+				response.eof ();
+			}
+		});
+
+		serverConnector.start ();
+		clientConnector.start ();
+
+		List<Future<String>> results = new ArrayList<> ();
+		for (int i = 0; i < 100; i++)
+		{
+			final String request = "message" + i;
+			results.add (requestExecutor.submit (new Callable<String> ()
+			{
+				@Override
+				public String call () throws Exception
+				{
+					byte[][] response = clientConnector.multipartRequest ("requestQueue", request.getBytes ());
+					List<String> result = new ArrayList<> ();
+					for (byte[] bytes : response)
+					{
+						result.add (new String (bytes));
+					}
+					return String.format ("%s|%s", request, Joiner.on (',').join (result));
+				}
+			}));
+		}
+
+		clientConnector.close ();
+		serverConnector.close ();
+
+		for (Future<String> result : results)
+		{
+			String[] parts = result.get ().split ("\\|");
+
+			assertThat (parts[1]).isEqualTo (String.format ("%1$s1,%1$s2,%1$s3", parts[0]));
+		}
+	}
+
+	@Test
+	public void testTimingOutRequest () throws Exception
+	{
+		serverConnector.setRequestHandler ("requestQueue", new RequestHandler ()
+		{
+			@Override
+			public byte[] onRequest (byte[] req)
+			{
+				try
+				{
+					Thread.sleep (1000);
+				}
+				catch (InterruptedException ignored)
+				{
+				}
+				return req;
+			}
+		});
+
+		serverConnector.start ();
+		clientConnector.start ();
+
+		try
+		{
+			clientConnector.request ("requestQueue", "still waiting".getBytes (), 100, TimeUnit.MILLISECONDS);
+			fail ("Should've timed out");
+		}
+		catch (Connector4Exception e)
+		{
+		}
+
+		clientConnector.close ();
+		serverConnector.close ();
+	}
+
+	@Test
+	public void testRequestReply () throws Exception
+	{
+		serverConnector.setRequestHandler ("requestQueue", new RequestHandler ()
+		{
+			@Override
+			public byte[] onRequest (byte[] req)
+			{
+				String reqString = new String (req);
+				return ("reply: " + reqString).getBytes ();
+			}
+		});
+
+		serverConnector.start ();
+		clientConnector.start ();
+
+		List<Future<String>> results = new ArrayList<> ();
+		for (int i = 0; i < 100; i++)
+		{
+			final String message = "hello" + i;
+			results.add (requestExecutor.submit (new Callable<String> ()
+			{
+				@Override
+				public String call () throws Exception
+				{
+					byte[] response = clientConnector.request ("requestQueue", message.getBytes ());
+					return message + "|" + new String (response);
+
+				}
+			}));
+		}
+
+		clientConnector.close ();
+		serverConnector.close ();
+
+		for (Future<String> result : results)
+		{
+			String[] parts = result.get ().split ("\\|");
+			assertThat ("reply: " + parts[0]).isEqualTo (parts[1]);
+		}
+	}
+
+	@Test
+	public void testNullReply () throws Exception
+	{
+		serverConnector.setRequestHandler ("requestQueue", new RequestHandler ()
+		{
+			@Override
+			public byte[] onRequest (byte[] req)
+			{
+				return null;
+			}
+		});
+
+		serverConnector.start ();
+		clientConnector.start ();
+
+		byte[] result = clientConnector.request ("requestQueue", "".getBytes ());
+		assertThat (result).isNull ();
+
+	}
+}
